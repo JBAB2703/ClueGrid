0df117bd3e60576955bb08dab2997b2612249ef7
JKyle <52245178+JBAB2703@users.noreply.github.com>
2025-05-03 10:48:49 -0400

    updated script for showing the correct word

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ if (!DEV_MODE && stats.lastPlayed === todayKey) {
 
 function lockGame(msg) {
   formEl.style.display = "none";
-  messageEl.textContent = msg;
+  messageEl.textContent += `<br>${msg}`;
 }
 
 function startGame() {
